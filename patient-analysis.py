7137ce7c99bfc483e84804dd558451f35dac255e
gardypr15 <gardypr13@gmail.com>
2017-06-20 11:08:44 -0400

    Added some more comments to the code

--- patient-analysis.py
+++ patient-analysis.py
@@ -1,8 +1,14 @@
+""" Patient's inflammation analysis for day 1 """"
+
 import numpy as np
 import matplotlib.pyplot as plt
  data = loadtxt(fnme='data/inflammation1.csv', delimeter =',')
+
+#Finding dimensions of data
+print(data.shape)
 
 print (data)
 image -1=plt.plot(data)
 plt.show(image-1)
 
+#Plotting data
